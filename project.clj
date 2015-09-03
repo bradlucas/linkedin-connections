541e8c6e70bd2e1a4fceb03b9f8532113054dc91
Brad Lucas <brad@beaconhill.com>
2015-09-03 18:08:39 -0400

    updated versions

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject org.clojars.blucas/linkedin-connections "1.0.0"
   :description "LinkedIn connections library"
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [clojure-csv/clojure-csv "1.3.2"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [clojure-csv/clojure-csv "2.0.1"]]
   :main linkedin-connections.core)
