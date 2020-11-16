8b7d8ea9ced2ffa5575d7a579505ea3ebfc32d1b
Brad Lucas <brad@beaconhill.com>
2020-11-15 22:26:56 -0500

    move to group com.bradlucas

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.blucas/linkedin-connections "1.0.2"
+(defproject com.bradlucas/linkedin-connections "1.0.2"
   :description "LinkedIn connections library"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [clojure-csv/clojure-csv "2.0.2"]]
